edd14fbebdd76ec212ede1ffbeb00f96dd325d04
Milan <milan@localhost.localdomain>
2019-08-14 20:54:13 -0400

    added stripe dependency

--- api.express/routes/index.js
+++ api.express/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("nothing to see here.");
+  res.send("Nothing to see here.");
 });
 
 module.exports = router;
