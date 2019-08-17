951b6181a04d04880eccee6f6ef80ca2ce68aa2a
Milan <milan@localhost.localdomain>
2019-08-16 22:19:28 -0400

    converted checkoutform to a react functional component

--- gui.react/src/stripe/CheckoutForm.js
+++ gui.react/src/stripe/CheckoutForm.js
@@ -1,15 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {CardElement, injectStripe} from 'react-stripe-elements';
 import styles from './CheckoutForm.module.css'
 
-class CheckoutForm extends Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
-  }
-
-  async submit(ev) {
-    let {token} = await this.props.stripe.createToken({name: "Name"});
+const CheckoutForm = props => {
+ 
+  const submit = async(ev) => {
+    let {token} = await props.stripe.createToken({name: "Name"});
     if (token !== undefined) {
       let response = await fetch("http://localhost:3000/checkout", {
           method: "POST",
@@ -20,16 +16,15 @@ class CheckoutForm extends Component {
       console.log(response);
       if (response.ok) alert("Transaction complete.");
     }  
-  }
+  };
 
-  render() {
-    return (
-      <div className={styles.CheckoutForm}>
-        <CardElement/>
-        <button onClick={this.submit}>SUBMIT</button>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.CheckoutForm}>
+      <CardElement/>
+      <button onClick={submit}>SUBMIT</button>
+    </div>
+  );
+  
 }
 
 export default injectStripe(CheckoutForm);
